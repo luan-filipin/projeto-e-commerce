f0117d2f8f596628d5f6595ef26655c91c365b29
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-18 13:57:23 -0300

    Produto-service
    - Primeiro commit funcional do serviço.
    - Realizado a configuração de conexão do banco de dados PostgreSQL.

---
+++ produto-server/src/test/java/br/com/e_commerce/produto_server/ProdutoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.e_commerce.produto_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
