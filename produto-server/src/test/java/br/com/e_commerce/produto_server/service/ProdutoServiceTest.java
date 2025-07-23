93a95636137e707a4ab7f36ac10f53b40a621a61
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-23 14:24:45 -0300

    Test Service
    - Criado o teste de sucesso e falha para o service que retornar todos os produtos paginados.

--- produto-server/src/test/java/br/com/e_commerce/produto_server/service/ProdutoServiceTest.java
+++ produto-server/src/test/java/br/com/e_commerce/produto_server/service/ProdutoServiceTest.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +24,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import br.com.e_commerce.produto_server.dto.ProdutoDto;
 import br.com.e_commerce.produto_server.dto.ProdutoRespostaCriacaoDto;
@@ -83,10 +92,10 @@ public class ProdutoServiceTest {
 		verify(produtoRepository).save(produtoEntity);
 		verify(produtoRespostaCriacaoMapper).toDto(produtoSalvo);
 	}
-	
+
 	@Test
 	public void deveFalharAoCriarUmProduto() {
-		
+
 		String codigo = "0";
 		String nome = "Mouse Gamer RGB";
 		String descricao = "Mouse ergonômico com iluminação RGB e 6 botões programáveis.";
@@ -95,21 +104,20 @@ public class ProdutoServiceTest {
 		String categoria = "Periféricos";
 		String imagemUrl = "https://minha-loja.com/imagens/produtos/mouse-gamer-rgb.jpg";
 
-		
 		ProdutoDto produtoEntrada = new ProdutoDto(codigo, nome, descricao, preco, quantidade, categoria, imagemUrl,
 				true);
-		
+
 		doThrow(new CodigoJaExisteException()).when(produtoValidador).validaSeCodigoJaFoiUsado(codigo);
-		
-		CodigoJaExisteException exception = assertThrows(CodigoJaExisteException.class, ()->{
+
+		CodigoJaExisteException exception = assertThrows(CodigoJaExisteException.class, () -> {
 			produtoServiceImp.criaProduto(produtoEntrada);
 		});
-		
+
 		assertEquals("Este codigo ja esta cadastrado!", exception.getMessage());
-		
+
 		verify(produtoRepository, never()).save(any());
 	}
-	
+
 	@Test
 	public void deveProcuraProdutoPeloCodigoComSucesso() {
 		// Arrange
@@ -121,41 +129,100 @@ public class ProdutoServiceTest {
 		String categoria = "Periféricos";
 		String imagemUrl = "https://minha-loja.com/imagens/produtos/mouse-gamer-rgb.jpg";
 
-		ProdutoDto produtoDto = new ProdutoDto(codigo, nome, descricao, preco, quantidade, categoria, imagemUrl,
-				true);
-		
+		ProdutoDto produtoDto = new ProdutoDto(codigo, nome, descricao, preco, quantidade, categoria, imagemUrl, true);
+
 		Produto produtoEntity = new Produto(1L, codigo, nome, descricao, preco, quantidade, categoria, imagemUrl, null,
 				null, true);
-		
+
 		when(produtoRepository.findByCodigo(codigo)).thenReturn(Optional.of(produtoEntity));
 		when(produtoMapper.toDto(produtoEntity)).thenReturn(produtoDto);
-		
+
 		// Act & Assert
 		ProdutoDto result = produtoServiceImp.procuraProdutoPeloCodigo(codigo);
-		
+
 		assertNotNull(result);
 		assertEquals(produtoDto, result);
 		assertEquals(produtoDto.codigo(), result.codigo());
 		assertEquals(produtoDto.nome(), result.nome());
-		
+
 		verify(produtoRepository).findByCodigo(codigo);
-		verify(produtoMapper).toDto(produtoEntity);	
+		verify(produtoMapper).toDto(produtoEntity);
 	}
-	
+
 	@Test
 	public void deveFalharAoProcurarProdutoPeloCodigo() {
 
 		String codigo = "0";
 
-	    when(produtoRepository.findByCodigo(codigo)).thenReturn(Optional.empty());
-		
-		CodigoNaoExisteException exception = assertThrows(CodigoNaoExisteException.class, ()->{
+		when(produtoRepository.findByCodigo(codigo)).thenReturn(Optional.empty());
+
+		CodigoNaoExisteException exception = assertThrows(CodigoNaoExisteException.class, () -> {
 			produtoServiceImp.procuraProdutoPeloCodigo(codigo);
 		});
-		
+
 		assertEquals("O codigo do produto não existe!", exception.getMessage());
-	    
+
 		verify(produtoMapper, never()).toDto(any());
+
+	}
+
+	@Test
+	public void deveRetornarTodosOsProdutos() {
+
+		Pageable pageable = PageRequest.of(0, 2);
+		LocalDateTime dataFixa = LocalDateTime.of(2024, 1, 1, 12, 0);
+
+		Produto produto1 = new Produto(1L, "PROD98231", "Fone Bluetooth X500",
+				"Fones de ouvido sem fio com cancelamento de ruído e bateria de longa duração.",
+				new BigDecimal("249.90"), 75, "Eletrônicos", "https://exemplo.com/imagens/fone-bluetooth.jpg", dataFixa,
+				dataFixa, true);
+
+		Produto produto2 = new Produto(1L, "PROD98831", "Fone Bluetooth teste",
+				"Fones de ouvido sem fio com cancelamento de ruído e bateria de longa duração.",
+				new BigDecimal("249.90"), 75, "Eletrônicos", "https://exemplo.com/imagens/fone-bluetooth.jpg", dataFixa,
+				dataFixa, true);
+
+		List<Produto> listaProduto = List.of(produto1, produto2);
+		Page<Produto> paginaProduto = new PageImpl<>(listaProduto);
+
+		ProdutoDto produtoDto1 = new ProdutoDto("PROD98231", "Fone Bluetooth X500",
+				"Fones de ouvido sem fio com cancelamento de ruído e bateria de longa duração.",
+				new BigDecimal("249.90"), 75, "Eletrônicos", "https://exemplo.com/imagens/fone-bluetooth.jpg", true);
+
+		ProdutoDto produtoDto2 = new ProdutoDto("PROD98831", "Fone Bluetooth teste",
+				"Fones de ouvido sem fio com cancelamento de ruído e bateria de longa duração.",
+				new BigDecimal("249.90"), 75, "Eletrônicos", "https://exemplo.com/imagens/fone-bluetooth.jpg", true);
+
+		when(produtoRepository.findAll(pageable)).thenReturn(paginaProduto);
+		when(produtoMapper.toDto(produto1)).thenReturn(produtoDto1);
+		when(produtoMapper.toDto(produto2)).thenReturn(produtoDto2);
+		
+		Page<ProdutoDto> resultado = produtoServiceImp.retornaTodosOsprodutos(pageable);
+		
+		assertNotNull(resultado);
+		assertEquals(2, resultado.getContent().size());
+		assertEquals("Fone Bluetooth X500", resultado.getContent().get(0).nome());
+		assertEquals("Fone Bluetooth teste", resultado.getContent().get(1).nome());
+		
+		verify(produtoRepository).findAll(pageable);
+		verify(produtoMapper).toDto(produto1);
+		verify(produtoMapper).toDto(produto2);
+	}
+	
+	@Test
+	public void deveRetornarPaginaVaziaQuandoNaoExistemProdutos() {
+		
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<Produto> paginaVazia = new PageImpl<>(Collections.emptyList());
+		
+		when(produtoRepository.findAll(pageable)).thenReturn(paginaVazia);
+		
+		Page<ProdutoDto> resultado = produtoServiceImp.retornaTodosOsprodutos(pageable);
+		
+		assertNotNull(resultado);
+		assertTrue(resultado.isEmpty());
+		
+		verify(produtoRepository, times(1)).findAll(pageable);
 		
 	}
 }
