e3bd01bc5b6805b2412615322cc2ff6a7ab61bff
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-24 17:42:00 -0300

    Controller
    - Criado ao controller para receber a requisição de deletar o produto.

--- produto-server/src/main/java/br/com/e_commerce/produto_server/controller/ProdutoController.java
+++ produto-server/src/main/java/br/com/e_commerce/produto_server/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,22 +27,28 @@ import lombok.RequiredArgsConstructor;
 public class ProdutoController {
 	
 	private final ProdutoService produtoService;
-
+	//Cria um produto
 	@PostMapping("/produtos")
 	public ResponseEntity<ProdutoRespostaCriacaoDto> criaProduto(@RequestBody @Valid ProdutoDto produtoDto) {
 		 ProdutoRespostaCriacaoDto produtoSalvao = produtoService.criaProduto(produtoDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvao);
 	}
-	
+	//Procura produto pelo codigo.
 	@GetMapping("/produtos/{codigo}")
 	public ResponseEntity<ProdutoDto> procuraProdutoPeloCodigo(@PathVariable @NotBlank String codigo){
 		ProdutoDto produto = produtoService.procuraProdutoPeloCodigo(codigo);
 		return ResponseEntity.ok(produto);
 	}
-	
+	//Retornar todos os produtos paginados.
 	@GetMapping("/produtos")
 	public ResponseEntity<Page<ProdutoDto>> retornaTodosOsProdutos(
 			@PageableDefault(page = 0, size = 20, sort = "nome", direction = Sort.Direction.ASC) Pageable pageable){
 		return ResponseEntity.ok(produtoService.retornaTodosOsprodutos(pageable));
+	}
+	//Deleta um produto pelo codigo.
+	@DeleteMapping("/produtos/{codigo}")
+	public ResponseEntity<Void> deletaProdutoPeloCodigo(@PathVariable @NotBlank String codigo) {
+		produtoService.deletaProdutoPeloCodigo(codigo);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
