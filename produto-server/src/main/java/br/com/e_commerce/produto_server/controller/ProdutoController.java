3cf492dd148cf27e540048674bebd7ab9740346b
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-28 16:26:58 -0300

    Controller
    - Criado o controller que vai receber a requisição para atualizar o produto.

--- produto-server/src/main/java/br/com/e_commerce/produto_server/controller/ProdutoController.java
+++ produto-server/src/main/java/br/com/e_commerce/produto_server/controller/ProdutoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class ProdutoController {
 		 ProdutoRespostaCriacaoDto produtoSalvao = produtoService.criaProduto(produtoDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvao);
 	}
-	
+	//Cria produtos em lote.
 	@PostMapping("/produtos/lote")
 	public ResponseEntity<List<ProdutoDto>> criaProdutosEmLote(@RequestBody  List<@Valid ProdutoDto> produtos){
 		List<ProdutoDto> produtosCriados = produtoService.criaProdutosEmLote(produtos);		
@@ -53,6 +54,14 @@ public class ProdutoController {
 			@PageableDefault(page = 0, size = 20, sort = "nome", direction = Sort.Direction.ASC) Pageable pageable){
 		return ResponseEntity.ok(produtoService.retornaTodosOsprodutos(pageable));
 	}
+	
+	//Atualiza o produto pelo codigo.
+	@PutMapping("/produtos/{codigo}")
+	public ResponseEntity<ProdutoDto> atualizaProdutoPeloCodigo(@PathVariable @NotBlank String codigo, @RequestBody @Valid ProdutoDto produtoDto){
+		ProdutoDto produtoAtualizado = produtoService.atualizaProdutoPeloCodigo(codigo, produtoDto);
+		return ResponseEntity.ok(produtoAtualizado);
+	}
+	
 	//Deleta um produto pelo codigo.
 	@DeleteMapping("/produtos/{codigo}")
 	public ResponseEntity<Void> deletaProdutoPeloCodigo(@PathVariable @NotBlank String codigo) {
