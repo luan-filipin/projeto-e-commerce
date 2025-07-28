ca2d1abf0f24d77b0946e164b28068f7340a158b
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-28 13:48:51 -0300

    GlobalExceptionHandler
    - Criando uma tratamento para Excepton que validam Argumentos.

--- produto-server/src/main/java/br/com/e_commerce/produto_server/config/GlobalExceptionHandler.java
+++ produto-server/src/main/java/br/com/e_commerce/produto_server/config/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package br.com.e_commerce.produto_server.config;
 
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.com.e_commerce.produto_server.dto.response.ErroCampoDto;
 import br.com.e_commerce.produto_server.dto.response.ErroRespostaDto;
 import br.com.e_commerce.produto_server.exception.CodigoJaExisteException;
 import br.com.e_commerce.produto_server.exception.CodigoNaoExisteException;
@@ -35,6 +39,20 @@ public class GlobalExceptionHandler {
 		ErroRespostaDto erro = new ErroRespostaDto(ex.getMessage(), HttpStatus.NOT_FOUND.value(),
 				request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErroRespostaDto> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request){
+		List<ErroCampoDto> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+				.map(error -> new ErroCampoDto(error.getField(), error.getDefaultMessage()))
+				.toList();
+		
+		ErroRespostaDto error = new ErroRespostaDto(
+				"Erro na validação",
+				HttpStatus.BAD_REQUEST.value(), 
+				request.getRequestURI(),
+				fieldErrors);
+		return ResponseEntity.badRequest().body(error);
 	}
 
 	
