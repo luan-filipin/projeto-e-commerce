b60230688e8c9b64ee89d1252c77b3748781ba36
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-29 15:30:28 -0300

    GlobalException
    - Refatorado o nome da exception para um nome em portugues.

--- user-service/src/main/java/br/com/user_service/config/GlobalExceptionHandler.java
+++ user-service/src/main/java/br/com/user_service/config/GlobalExceptionHandler.java
@@ -5,14 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.user_service.exception.LoginAlreadyExistsException;
+import br.com.user_service.exception.LoginJaExiste;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler
-	public ResponseEntity<String> handleLoginAlreadyExists(LoginAlreadyExistsException ex){
+	public ResponseEntity<String> handleLoginAlreadyExists(LoginJaExiste ex){
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
 	}
 
