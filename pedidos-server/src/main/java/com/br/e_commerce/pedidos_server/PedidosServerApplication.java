a2d830373c4ada1045b4c8afae69602cba9a97d5
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-30 11:54:48 -0300

    Primeiro commit
    - Configurado a conexão com o banco via variaveis de ambiente.

---
+++ pedidos-server/src/main/java/com/br/e_commerce/pedidos_server/PedidosServerApplication.java
@@ -0,0 +1,13 @@
+package com.br.e_commerce.pedidos_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosServerApplication.class, args);
+	}
+
+}
