1dc3adee34c2a57745029ae2146f1f5e0363a929
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-31 16:41:12 -0300

    Security
    - Criado e configurado a segurança da rota para acesso do mesmo.

---
+++ pedidos-server/src/main/java/com/br/e_commerce/pedidos_server/security/ConfigSecurity.java
@@ -0,0 +1,26 @@
+package com.br.e_commerce.pedidos_server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurity {
+
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/api/pedidos").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
